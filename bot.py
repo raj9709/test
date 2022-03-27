8dcfb6f7187a8b8ed51eaf7ecb7238188b327a0d
raj9709 <98457958+raj9709@users.noreply.github.com>
2022-03-27 22:57:30 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,7 @@
+from http import client
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '&')
+
+client.run("OTIzNzk0OTE0MzI2NzQxMDYz.YcVNDg.ABgQC3VPSnD7ep3WC0Gz-8xe80M")
